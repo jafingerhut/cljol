73b870f2c0b1ca244ae9f85f594e9f7aff03b112
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-06-27 23:18:45 -0700

    Updates to script for generating images

--- doc/generate-images.sh
+++ doc/generate-images.sh
@@ -1,3 +1,29 @@
 #! /bin/bash
 
-clojure -m cljol.generate
+# Remember the current directory when the script was started:
+INSTALL_DIR="${PWD}"
+
+# Found here:
+#
+# https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself/20265654
+#
+# I tested it on Ubuntu 18.04 Linux and macOS 10.13.6 and it worked
+# for both, so at least for now I am going for it.
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+#echo ":${SCRIPTPATH}:"
+#exit 0
+
+OUTPUT_DIR="${SCRIPTPATH}/images"
+mkdir -p "${OUTPUT_DIR}"
+
+clojure -m cljol.generate "${OUTPUT_DIR}"
+
+cd "${OUTPUT_DIR}"
+for j in *.dot
+do
+    echo "Generating images from $j ..."
+    for format in png pdf
+    do
+	dot -T${format} "$j" -o `basename $j .dot`.${format}
+    done
+done
--- src/cljol/generate.clj
+++ src/cljol/generate.clj
@@ -1,5 +1,6 @@
 (ns cljol.generate
   (:gen-class)
+  (:import (java.io File))
   (:require [cljol.dig9 :as d]
             [clojure.string :as str])) 
 
@@ -20,46 +21,82 @@
 (def stack-desc (str/replace (str os-desc "-" jvm-desc "-" clj-desc)
                              " " "-"))
 
-(defn fname [s]
-  (str s "-" stack-desc ".dot"))
+(defn fname [s opts]
+  (str (:output-dir opts) File/separator
+       s "-" stack-desc ".dot"))
 
-(def opts-default {})
-(def opts-with-field-values {:label-node-with-field-values? true})
 
-;; Avoid calling clojure.core/str or any similar function on a lazy
-;; sequence if you do not want it to be realized.
-(def opts-dont-realize-values (merge opts-with-field-values
-                                     {:node-label-fn (constantly "")}))
-
-(def opts opts-with-field-values)
-
-(defn gen [obj name & other-args]
+(defn gen [obj name opts]
   (println "Generating" name "...")
-  (apply d/write-dot-file obj (fname name) other-args))
+  (d/write-dot-file obj (fname name opts) opts))
+
+
+(defn parse-args [args]
+  (when (not= 1 (count args))
+    (binding [*out* *err*]
+      (println (format "usage: %s <directory-to-write-dot-fiels>" *file*))
+      (System/exit 1)))
+  {:output-dir (nth args 0)})
 
 
 (defn -main [& args]
-  (println "Hello world!")
+  (let [cmdline-opts (parse-args args)
+        opts-default cmdline-opts
+        opts-with-field-values (merge cmdline-opts
+                                      {:label-node-with-field-values? true})
 
-  (let [map1 (let [x :a y :b] {x y y x})]
-    (gen map1 "map1" opts))
+        ;; Avoid calling clojure.core/str or any similar function on a
+        ;; lazy sequence if you do not want it to be realized.
+        opts-dont-realize-values (merge opts-with-field-values
+                                        {:node-label-fn (constantly "")})
+        opts opts-with-field-values]
 
-  ;; Show effects of a lazy sequence being generated on demand,
-  ;; without chunking.
+    (let [map1 (let [x :a y :b] {x y y x})]
+      (gen map1 "map1" opts))
 
-  ;; Creators of sequences: repeat, range
-
-  ;; Functions in core.clj that have special code to handle chunked
-  ;; sequences in:
-  ;; reduce1
-  ;; reverse - from reduce1
-  ;; A _lot_ of functions in core use reduce1
+    ;; Show effects of a lazy sequence being generated on demand,
+    ;; without chunking.
+    
+    ;; Creators of sequences: repeat, range
+    
+    ;; Functions in core.clj that have special code to handle chunked
+    ;; sequences in:
+    ;; reduce1
+    ;; reverse - from reduce1
+    ;; A _lot_ of functions in core use reduce1
   
-  ;; sequence
-  ;; map map-indexed filter remove (inherited from filter) keep keep-indexed
-  ;; random-sample - from filter
-  ;; doseq
-  ;; iterator-seq
+    ;; sequence
+    ;; map map-indexed filter remove (inherited from filter) keep keep-indexed
+    ;; random-sample - from filter
+    ;; doseq
+    ;; iterator-seq
   
-  (let [
-  )
+    ))
+
+
+(comment
+
+(do
+(use 'cljol.generate)
+(require '[cljol.dig9 :as d])
+)
+
+;; Interesting!  Self-loop for optimal memory efficiency!
+(def lazy2 (repeat 42))
+(d/view lazy2 opts-dont-realize-values)
+(take 1 lazy2)
+(take 10 lazy2)
+
+;; Generates a linked list of a Repeat object, each with a count 1
+;; less than the one before.
+(def lazy3 (repeat 10 "a"))
+(d/view lazy3 opts-dont-realize-values)
+(take 1 lazy3)
+(take 4 lazy3)
+
+(def lazy4 (seq (vec (range 100))))
+(d/view lazy4 opts-dont-realize-values)
+(take 1 lazy4)
+(take 4 lazy4)
+
+)
