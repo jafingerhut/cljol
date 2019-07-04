3e5d45511cc45695eab8d841e0a77b6a3bf4b437
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-07-04 16:58:49 -0700

    Change source paths around a bit, so generate code is normally not present
    Add a new :generate alias to deps.edn to add src/generate as an
    option.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/jafingerhut/cljol"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.openjdk.jol/jol-core "0.9"]
                  [ubergraph "0.5.3"]
