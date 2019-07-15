63215d2fad59ad4bfcb4d45d3e50f350528395d7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-07-15 11:20:51 -0700

    Faster code in scratch1.clj to see how much some heuristics ...
    might help reduce the object graph size, while preserving the ability
    to calculate correct reachable number and total size of nodes.

--- doc/scratch1.clj
+++ doc/scratch1.clj
@@ -70,14 +70,32 @@
 (def sccg nil)
 (gsize sccg)
 (uber/pprint sccg)
-(def sccg2 (gr/reduce-dag-experiment1 sccg))
+(d/view-graph sccg)
+(def sccg1 (reduce-dag-experiment1 sccg))
+(def sccg1 nil)
+(gsize sccg1)
+(uber/pprint sccg1)
+(d/view-graph sccg1)
+(def sccg2 (reduce-dag-experiment2 sccg))
 (def sccg2 nil)
 (gsize sccg2)
 (uber/pprint sccg2)
 
-(count (filter #(and (= 1 (count (uber/successors sccg2 %)))
-                     (= 1 (count (uber/predecessors sccg2 %))))
-               (uber/nodes sccg2)))
+(def sccg3 (reduce-dag-experiment2 sccg1))
+(gsize sccg3)
+
+(count (filter #(and (= 1 (count (uber/successors sccg1 %)))
+                     (= 1 (count (uber/predecessors sccg1 %))))
+               (uber/nodes sccg1)))
+
+;; Example results
+
+;; (gsize g)     126278 nodes 266227 edges
+;; (gsize sccg)  104210 nodes 173976 edges
+;; (gsize sccg1)  65602 nodes 135368 edges (38608 fewer nodes & edges)
+;; (gsize sccg2) 104210 nodes 173976 edges (0 fewer nodes & edges)
+;; (count ...)    32599 nodes with in-deg=out-deg=1 in sccg1
+
 (def f (frequencies
         (map (fn [v]
                [(count (uber/predecessors sccg2 v))
@@ -123,6 +141,7 @@ n1
 
 (def g3 (gr/induced-subgraph g (filter #(<= (uber/attr g % :distance) 6)
                                         (uber/nodes g))))
+(gsize g3)
 (d/view-graph g3)
 (d/view-graph g3 {:save {:filename "g3.pdf" :format :pdf}})
 
@@ -444,46 +463,60 @@ g1
 
   Assumes that the input graph g contains no cycles."
   [g]
-  (let [in-0-out-1 (filter #(and (= 0 (count (uber/predecessors g %)))
-                                  (= 1 (count (uber/successors g %))))
-                            (uber/nodes g))
-         in-1-out-0 (filter #(and (= 1 (count (uber/predecessors g %)))
-                                  (= 0 (count (uber/successors g %))))
-                            (uber/nodes g))]
-    (loop [in-0-out-1 in-0-out-1
-           in-1-out-0 in-1-out-0
-           g2 g]
-;;      (println "in-0-out-1" in-0-out-1)
+  (let [outdeg (into {} (for [n (uber/nodes g)]
+                          [n (uber/out-degree g n)]))
+;;        _ (println "outdeg=" outdeg)
+        in-1-out-0 (filter #(and (= 1 (uber/in-degree g %))
+                                 (= 0 (uber/out-degree g %)))
+                           (uber/nodes g))]
+    (loop [in-1-out-0 in-1-out-0
+           outdeg outdeg
+           removed-nodes (transient [])]
 ;;      (println "in-1-out-0" in-1-out-0)
-      (cond
-        (next in-0-out-1)  ;; true if at least 1 node in this list
+      (if (first in-1-out-0)  ;; doesn't support nil as a node, but oh well
+        (let [w (first in-1-out-0)
+              v (first (uber/predecessors g w))
+;;              _ (println "removing w=" w " with predecessor v=" v
+;;                         " it had outdeg=" (outdeg v))
+              outdeg (update outdeg v dec)
+              new-node? (and (zero? (outdeg v))
+                             (= 1 (uber/in-degree g v)))]
+          ;;(println "remove" w "new-node?" new-node? "neighbor" v)
+          (recur (if new-node?
+                   (cons v (rest in-1-out-0))
+                   (rest in-1-out-0))
+                 outdeg
+                 (conj! removed-nodes w)))
+        ;; else
+        (uber/remove-nodes* g (persistent! removed-nodes))))))
+
+
+(defn reduce-dag-experiment2
+  "Experimental code to see how much smaller it makes DAGs that are
+  returned by scc-graph.  If it gives significant enough reductions,
+  it could enable linear time calculation of reachable-node-stats for
+  a larger class of object graphs.
+
+  Assumes that the input graph g contains no cycles."
+  [g]
+  (let [indeg (into {} (for [n (uber/nodes g)]
+                         [n (uber/in-degree g n)]))
+        in-0-out-1 (filter #(and (= 0 (uber/in-degree g %))
+                                 (= 1 (uber/out-degree g %)))
+                           (uber/nodes g))]
+    (loop [in-0-out-1 in-0-out-1
+           indeg indeg
+           removed-nodes (transient [])]
+      (if (first in-0-out-1)  ;; doesn't support nil as a node, but oh well
         (let [v (first in-0-out-1)
-              _ (assert (= 1 (count (uber/successors g2 v))))
-              _ (assert (= 0 (count (uber/predecessors g2 v))))
-              w (first (uber/successors g2 v))
-              _ (assert (>= (count (uber/predecessors g2 w)) 1))
-              new-node? (and (= 1 (count (uber/predecessors g2 w)))
-                             (= 1 (count (uber/successors g2 w))))]
-;;          (println "remove" v "new-node?" new-node? "neighbor" w)
+              w (first (uber/successors g v))
+              indeg (update indeg w dec)
+              new-node? (and (zero? (indeg w))
+                             (= 1 (uber/out-degree g w)))]
           (recur (if new-node?
                    (cons w (rest in-0-out-1))
                    (rest in-0-out-1))
-                 in-1-out-0
-                 (uber/remove-nodes g2 v)))
-        
-        (next in-1-out-0)  ;; true if at least 1 node in this list
-        (let [w (first in-1-out-0)
-              _ (assert (= 0 (count (uber/successors g2 w))))
-              _ (assert (= 1 (count (uber/predecessors g2 w))))
-              v (first (uber/predecessors g2 w))
-              _ (assert (>= (count (uber/successors g2 v)) 1))
-              new-node? (and (= 1 (count (uber/predecessors g2 v)))
-                             (= 1 (count (uber/successors g2 v))))]
-;;          (println "remove" w "new-node?" new-node? "neighbor" v)
-          (recur in-0-out-1
-                 (if new-node?
-                   (cons v (rest in-1-out-0))
-                   (rest in-1-out-0))
-                 (uber/remove-nodes g2 w)))
-        
-        :else g2))))
+                 indeg
+                 (conj! removed-nodes v)))
+        ;; else
+        (uber/remove-nodes* g (persistent! removed-nodes))))))
