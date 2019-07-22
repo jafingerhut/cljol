55a5dcb01fd97cfe3fee1927e4cadc0902978f47
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-07-21 22:06:33 -0700

    Add some tests for calculating SCCs

--- test/cljol/dig_test.clj
+++ test/cljol/dig_test.clj
@@ -1,6 +1,11 @@
 (ns cljol.dig-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [loom.alg :as lalg]
             [ubergraph.core :as uber]
+            [cljol.performance :as perf]
+            [cljol.graph :as gr]
             [cljol.dig9 :refer :all]))
 
 (def ref-array (object-array 5))
@@ -44,3 +49,52 @@
     (is (= #{1 4} (set (uber/predecessors g 2))))
     (is (apply distinct? (uber/predecessors g 2)))
     (is (= false (apply distinct? (map uber/src (uber/in-edges g 2)))))))
+
+
+(defn compare-sccs-with-perf [g]
+  (println "Comparing SCCs found multiple ways for graph with"
+           (uber/count-nodes g) "nodes,"
+           (uber/count-edges g) "edges")
+  (let [{loom-scc :ret :as p} (perf/my-time (lalg/scc g))
+        _ (do (print "Using loom.alg/scc, found" (count loom-scc) "SCCs in: ")
+              (perf/print-perf-stats p))
+        {scc-data :ret :as p} (perf/my-time (gr/scc-tarjan g))
+        _ (do (print "Using cljol.graph/scc-tarjan, found"
+                     (count (:components scc-data)) "SCCs in: ")
+              (perf/print-perf-stats p))]
+    (is (= (set (map set loom-scc))
+           (set (:components scc-data))))))
+
+
+(deftest scc-tests
+  (let [g1 (uber/multidigraph
+	    [1 2]
+	    [3 4]
+	    [5 6]
+	    [5 7]
+	    [8 4]
+	    [8 2]
+	    [9 5]
+	    [9 8]
+	    [2 10]
+	    [2 3]
+	    [4 10]
+	    [4 1])
+        fname2 "resources/dimultigraph-129k-nodes-272k-edges.edn"
+        {edges :ret :as p} (perf/my-time
+                            (with-open [rdr (java.io.PushbackReader.
+                                             (io/reader fname2))]
+                              (edn/read rdr)))
+        _ (do (print "Read" (count edges) "in: ")
+              (perf/print-perf-stats p))
+        {g2 :ret :as p} (perf/my-time
+                         (-> (uber/multidigraph)
+                             (uber/add-edges* edges)))
+        _ (do (print "Created graph with" (uber/count-nodes g2) "nodes,"
+                     (uber/count-edges g2)" edges in: ")
+              (perf/print-perf-stats p))
+        ]
+    (is (= (set (map set (lalg/scc g1)))
+           (set (:components (gr/scc-tarjan g1)))))
+    (compare-sccs-with-perf g1)
+    (compare-sccs-with-perf g2)))
