f72e63837af4cf2f2f3d935c02d498b6291ceec7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-07-27 11:58:36 -0700

    Update project.clj file also to use latest cljol-jvm-support

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cljol "0.2.0"
+(defproject cljol "0.3.0"
   :description "Analyze Java objects in memory using the Java Object Layout library"
   :url "http://github.com/jafingerhut/cljol"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.fingerhutpress.cljol_jvm_support/cljol_jvm_support "0.9"]
+                 [com.fingerhutpress.cljol_jvm_support/cljol_jvm_support "1.0"]
                  [ubergraph "0.6.1"
                   :exclusions [tailrecursion/cljs-priority-map]]]
   :profiles {:uberlocal {:dependencies [[ubergraph "0.5.4-andy-mods"]]}
